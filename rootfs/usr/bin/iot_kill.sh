7bd985a0214a2de72b91f0f6864b0c9024236453
taidalab <zdajun1988@163.com>
2017-11-02 17:00:52 +0800

    重新调整文件系统,第一发布版本

---
+++ rootfs/usr/bin/iot_kill.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# ------------------------------------------------------------------
+# Author:    nlv10677
+# Copyright: NXP B.V. 2014. All rights reserved
+# ------------------------------------------------------------------
+
+
+echo "Kill all IoT processes"
+
+killbyname iot_monitor
+killbyname iot_ed
+killbyname iot_ci
+killbyname iot_sci
+killbyname iot_sm
+killbyname iot_acs
+killbyname iot_su
